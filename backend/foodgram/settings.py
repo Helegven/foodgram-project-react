58304ca2bd18bc972541ac27dc02fec8174bc825
Кондратенко Александр <helegven@yandex.ru>
2023-07-24 16:26:38 +1000

    wip

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,6 +12,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '0.0.0.0',
     'localhost',
+    'backend',
     'foodisgood.ddns.net'
 ]
 
@@ -114,8 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# STATIC_URL = '/backend_static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -129,6 +133,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'SEARCH_PARAM': 'name'
 }
 
