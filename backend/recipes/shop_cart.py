86bf5c640104bd61dd6ba6b2ece7712b50972778
Кондратенко Александр <helegven@yandex.ru>
2023-08-06 17:18:41 +1000

    flake8

--- backend/recipes/shop_cart.py
+++ backend/recipes/shop_cart.py
@@ -10,18 +10,20 @@ def get_shopping_cart(request):
             recipe__in_shopping_list__user=request.user
         ).values('ingredient').annotate(
             total_amount=Sum('amount')
-            ).values_list('ingredient__name',
-                          'total_amount',
-                          'ingredient__measurement_unit'
-                          )
+            ).values_list(
+                'ingredient__name',
+                'total_amount',
+                'ingredient__measurement_unit'
+            )
     )
     buy_list_text = []
     [buy_list_text.append(
         '{} - {} {}.'.format(*ingredient)) for ingredient in ingredients]
-    response = HttpResponse('Список покупок с сайта Foodgram:\n'
-                            + '\n'.join(buy_list_text),
-                            content_type='text/plain'
-                            )
+    response = HttpResponse(
+        'Список покупок с сайта Foodgram:\n'
+        + '\n'.join(buy_list_text),
+        content_type='text/plain'
+    )
     response['Content-Disposition'] = (
         'attachment; filename=shopping-list.txt'
         )
